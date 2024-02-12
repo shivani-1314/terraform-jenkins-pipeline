65e1cc784a8112f070a05be856ee00271916b25c
shivani-1314 <142994273+shivani-1314@users.noreply.github.com>
2024-02-12 15:12:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,14 +13,14 @@ variable "aws_secret_key" {
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-2"
 }
 
 
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
+   default     = "ami-05fb0b8c1424f266b"
 }
 
 variable "instance_type" {
